83eb0946e5aa3f85a74581cd77cae79cf80de5e3
PavloYurchyshynn <gamepaha123@gmail.com>
2022-10-26 16:01:16 +0300

    fix: json-server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import { jsonServer } from 'json-server';
+import jsonServer from 'json-server';
+
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults({
