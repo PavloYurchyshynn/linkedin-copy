00b1e787bc5c815790799671e9e5b6caa00dd761
PavloYurchyshynn <gamepaha123@gmail.com>
2022-10-26 16:32:02 +0300

    fix: eslint rules

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-import jsonServer from 'json-server';
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults({
