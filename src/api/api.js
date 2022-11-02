cdaff43b420cd1672347548e32bc2d46f3441f12
PavloYurchyshynn <gamepaha123@gmail.com>
2022-11-02 17:15:38 +0200

    fix: api bug

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: true,
-  // baseURL: 'http://localhost:8000/'
-  baseURL: 'https://linkedin-copy.herokuapp.com/api/'
+  baseURL: 'http://localhost:8000/'
+  // baseURL: 'https://linkedin-copy.herokuapp.com/api/'
 });
 
 export const usersAPI = {
